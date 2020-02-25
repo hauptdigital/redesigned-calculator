4d3187f666142b6ca531e8d6a40e6fca14047bae
hauptdigital <marc@haupt.digital>
2020-02-25 17:24:50 +0100

    Add basic js
    Added eventlistener click
    
    Added calculate() function with switch for all operation types

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,49 @@
-let calcinputs = document.querySelector(".calculator__input");
+function calculate(number, operation) {
+  switch (operation) {
+    case "number":
+      console.log("number");
+      break;
+    case "plus":
+      console.log("plus");
+      break;
+    case "minus":
+      console.log("minus");
+      break;
+    case "plus":
+      console.log("plus");
+      break;
+    case "divide":
+      console.log("divide");
+      break;
+    case "multiply":
+      console.log("multiply");
+      break;
+    case "plus":
+      console.log("plus");
+      break;
+    case "equal":
+      console.log("equal");
+      break;
+    case "clear":
+      console.log("clear");
+      break;
+    case "allclear":
+      console.log("allclear");
+      break;
+  }
+}
 
-calcinputs.addEventListener("click", function() {
-  alert("clicked");
-});
+let calculatorControls = document.querySelectorAll(
+  ".calculator__input, .calculator__operator"
+);
+
+calculatorControls.forEach(control =>
+  control.addEventListener("click", function() {
+    let operation = control.dataset.type;
+    let number = "";
+    if (control.dataset.type == "number") {
+      number = parseInt(control.innerHTML);
+    }
+    calculate(number, operation);
+  })
+);
