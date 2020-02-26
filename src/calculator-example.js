3bbac4a7e9077b480ed478d89e93a4582d52b44e
hauptdigital <marc@haupt.digital>
2020-02-26 14:29:22 +0100

    Add result and clear functionality

--- src/calculator-example.js
+++ src/calculator-example.js
@@ -1,7 +1,10 @@
 "use strict";
 
+const calculatorResult = document.querySelector(".calculator__button-equal");
+const calculatorClear = document.querySelector(".calculator__button-clear");
+
 const calculatorOutput = document.querySelector("#result");
-calculatorOutput.value = plus();
+calculatorOutput.value = plus(2, 3);
 
 function plus(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -15,3 +18,21 @@ function divide(numberOne, numberTwo) {
 function multiply(numberOne, numberTwo) {
   return numberOne * numberTwo;
 }
+
+let operandOne = 4;
+let operandTwo = 5;
+
+function printResult() {
+  calculatorOutput.value = plus(operandOne, operandTwo);
+}
+
+function clearField() {
+  calculatorOutput.value = 0;
+}
+
+/* Bind events to click handlers */
+calculatorResult.addEventListener("click", printResult);
+calculatorClear.addEventListener("click", clearField);
+
+/* Initial reset of field on load */
+clearField();
